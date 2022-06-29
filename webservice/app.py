a2a6cf9fd7dd7c42f1c3d45d23b5709074c83c73
kaggis <kaggis@gmail.com>
2022-06-29 17:17:45 +0300

    REC-63 Serve individual statistics and metrics from webservice api (#30)
    * REC-55 Reorganize metrics output
    
    * REC-55 Reorganize metrics output
    
    * REC-62 Include gini index in the report ui
    
    * REC-63 Serve individual statistics and metrics from webservice api

--- webservice/app.py
+++ webservice/app.py
@@ -18,11 +18,62 @@ def main_page():
 
 
 @app.route("/api")
-def api_metrics():
-    '''Serve the metrics data in json format as a default api response'''
+def get_api_index():
+    '''Serve metrics and statistics as default api response'''
     result = {}
 
     with open(app.config['RS_EVALUATION_METRICS'], 'r') as f:
       result = json.load(f)
       f.close()
     return jsonify(result)
+
+
+@app.route("/api/metrics")
+def get_metrics():
+    '''Serve the metrics data in json format'''
+    result = {}
+
+    with open(app.config['RS_EVALUATION_METRICS'], 'r') as f:
+      result = json.load(f)
+      f.close()
+    return jsonify(result['metrics'])
+
+@app.route("/api/metrics/<string:metric_name>")
+def get_metric(metric_name):
+    '''Serve specific metric data in json format'''
+    result = {}
+
+    with open(app.config['RS_EVALUATION_METRICS'], 'r') as f:
+      result = json.load(f)
+      f.close()
+    
+    for metric in result['metrics']:
+      if metric['name'] == metric_name:
+        return jsonify(metric)
+    
+    return jsonify({'code':404,'error':'metric with name: {} does not exist!'.format(metric_name)}),404
+
+@app.route("/api/statistics")
+def get_statistics():
+    '''Serve the statistics data in json format'''
+    result = {}
+
+    with open(app.config['RS_EVALUATION_METRICS'], 'r') as f:
+      result = json.load(f)
+      f.close()
+    return jsonify(result['statistics'])
+
+@app.route("/api/statistics/<string:stat_name>")
+def get_statistic(stat_name):
+    '''Serve specific statistic data in json format'''
+    result = {}
+
+    with open(app.config['RS_EVALUATION_METRICS'], 'r') as f:
+      result = json.load(f)
+      f.close()
+    
+    for stat in result['statistics']:
+      if stat['name'] == stat_name:
+        return jsonify(stat)
+    
+    return jsonify({'code':404,'error':'metric with name: {} does not exist!'.format(stat_name)}),404
