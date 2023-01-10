d2fbf61720ef75326586275c369def16cce9ef15
Konstantinos Kagkelidis <kaggis@gmail.com>
2023-01-10 10:12:55 +0200

    REC-167 Add basic routing and placeholders for main views

--- webservice/rseval-ui/src/App.js
+++ webservice/rseval-ui/src/App.js
@@ -1,25 +1,74 @@
-import logo from './logo.svg';
 import './App.css';
+import logo from './grnet.png';
+import * as React from 'react';
+import { Routes, Route, Outlet, NavLink } from 'react-router-dom';
 
-function App() {
+
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Routes>
+      <Route element={<Layout />}>
+        <Route index element={<Metrics />} />
+        <Route path="kpis" element={<KPIS />} />
+        <Route path="graphs" element={<Graphs />} />
+        {/* TODO: Add dynamic routes for documentation views */}
+      </Route>
+    </Routes>
   );
-}
+};
 
+const Layout = () => {
+  
+
+  return (
+    <>
+      <section className="wrapper">
+      <aside>
+        <nav>
+          <ul>
+            <li><img src={logo} alt="logo" className="logo"/></li>
+            <li><span className="nav-category">METRICS DASHBOARD</span></li>
+            <li><NavLink to="/">Metrics</NavLink></li>
+            <li><NavLink to="/kpis">KPIs</NavLink></li>
+            <li><NavLink to="/graphs">Graphs</NavLink></li>
+            <li><span className="nav-category">METRICS DOCUMENTATION</span></li>
+          </ul>
+        </nav>
+      </aside>
+      <main>
+        <Outlet />
+      </main>
+      </section>
+    </>
+  );
+};
+
+// This is the main metrics view component - can be extended to its own file
+function Metrics() {
+  return (
+    <>
+      <h2>Metrics</h2>
+    </>
+  );
+};
+
+// This is the KPIS view component - can be extended to its own file
+function KPIS() {
+  return (
+    <>
+      <h2>KPIs</h2>
+    </>
+  );
+};
+
+// This is the Graphs view component - can be extended to its own file
+function Graphs() {
+  return (
+    <>
+      <h2>Graphs</h2>
+    </>
+  );
+};
+
+// TODO: Add component to generate dynamic views for metrics documentation
 export default App;
